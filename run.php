56178df6eeab98130ee2f85bf536238e062ce210
pengdong3 <pengdong3@AndrewdeMacBook-Pro.local>
2017-04-03 12:48:12 +0800

    change

--- run.php
+++ run.php
@@ -4,6 +4,6 @@ require 'vendor/autoload.php';
 use Stichoza\GoogleTranslate\TranslateClient;
 
 $tr = new TranslateClient();
-$src = file_get_contents('src.txt');
+$src = file_get_contents('data/src.txt');
 $dest = $tr->setSource('en')->setTarget('zh-CN')->translate($src);
-file_put_contents('result.txt', $dest);
+file_put_contents('data/result.txt', $dest);
